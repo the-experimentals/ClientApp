3b0cfb5eb816d37f4d7da327fb7fd3a4c7de0532
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-06 20:14:45 +0530

    Auth home layout

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthHomeComponent } from './auth-home/auth-home.component';
+import { AuthSignInComponent } from './auth-sign-in/auth-sign-in.component';
 
 const routes: Routes = [{
   path:'',
-  component: AuthHomeComponent
+  component: AuthHomeComponent,
+  children:[{
+    path: '',
+    component: AuthSignInComponent
+  }]
 }];
 
 @NgModule({
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthHomeComponent } from './auth-home/auth-home.component';
 import { AuthSignInComponent } from './auth-sign-in/auth-sign-in.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [AuthHomeComponent, AuthSignInComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class AuthModule { }
