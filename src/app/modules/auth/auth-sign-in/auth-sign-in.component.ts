2aa50ac5258d3622f793513a5b78643422b58e7e
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-06 23:56:05 +0530

    Sign in page validations

--- src/app/modules/auth/auth-sign-in/auth-sign-in.component.ts
+++ src/app/modules/auth/auth-sign-in/auth-sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { faEyeSlash, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import { ValidateOnValueChange } from 'src/app/core/validators/form-field-validator';
 
 @Component({
   selector: 'app-auth-sign-in',
@@ -13,9 +14,33 @@ export class AuthSignInComponent implements OnInit {
   faLock = faLock
   faEye = faEyeSlash
 
+  signInError!:string[];
+  showSignInErrors:boolean = false;
+  isSignInAttempt:boolean = false;
+
+  validationMessages = {
+    'IDENTIFIER': {
+      'required': 'You must enter data in username',
+      'minlength':'Your username cannot be lesser than 5 characters'
+    },    
+    'SECRET':{
+      'required': 'You must enter data in password',
+      'minlength':'Your password cannot be lesser than 8 characters'
+    }
+  };
+
+  signInFormErrors = {
+    'IDENTIFIER': this.validationMessages['IDENTIFIER']['required'],
+    'SECRET': this.validationMessages['SECRET']['required']
+  }
+
   signInForm: FormGroup
   constructor(private formBuilder:FormBuilder) { 
     this.signInForm = this.createSignInForm()
+
+    this.signInForm.valueChanges.subscribe(data => {
+      ValidateOnValueChange(this.signInForm, this.signInFormErrors, this.validationMessages, data);
+    });
   }
 
   ngOnInit(): void {
@@ -23,10 +48,20 @@ export class AuthSignInComponent implements OnInit {
 
   createSignInForm(): FormGroup{
     return this.formBuilder.group({
-      'IDENTIFIER': new FormControl('',[Validators.required]),
-      'SECRET': new FormControl('', [Validators.required]),
+      'IDENTIFIER': new FormControl('',[Validators.required, Validators.minLength(5)]),
+      'SECRET': new FormControl('', [Validators.required, Validators.minLength(8)]),
       'REMEMBER_ME': new FormControl(false)
     });
+  }
+
+  get IDENTIFIER()
+  {
+    return this.signInForm.get('IDENTIFIER');
+  }
+
+  get SECRET()
+  {
+    return this.signInForm.get('SECRET');
   }
 
   passwordToggle(){
