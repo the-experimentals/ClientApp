44a84fedcbe55423c3543f3f154147a9cacf97e9
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-06 19:54:17 +0530

    Auth module and components

---
+++ src/app/modules/auth/auth-sign-in/auth-sign-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-sign-in',
+  templateUrl: './auth-sign-in.component.html',
+  styleUrls: ['./auth-sign-in.component.scss']
+})
+export class AuthSignInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthHomeComponent } from './auth-home/auth-home.component';
+import { AuthSignInComponent } from './auth-sign-in/auth-sign-in.component';
+
+
+@NgModule({
+  declarations: [AuthHomeComponent, AuthSignInComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
