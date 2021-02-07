bd769d9b49bbcecf878c4e19a7a513ee99ca4672
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-07 23:12:29 +0530

    Redirect unauthorised user to login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './core/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Task Manager';
+
+  constructor(private authService:AuthService, private router:Router){}
+
+  ngOnInit(){
+    this.authService.isAuthenticated().subscribe(authenitcated => {
+      if(!authenitcated)
+        this.router.navigate(['']);
+    })
+  }
 }
