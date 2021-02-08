8503847f872365ae539bcec0e36100f0732c6bff
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-08 21:11:33 +0530

    Error interceptor

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -5,7 +5,9 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { StatusCodes } from 'http-status-codes';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -13,6 +15,18 @@ export class ErrorInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request)
+      .pipe(catchError(err =>{
+        console.log(err)
+        switch(err.status){
+          case 0:
+            // service not reachable
+          case StatusCodes.INTERNAL_SERVER_ERROR:
+            break;
+          default:
+            // do nothing
+        }
+        return throwError(err)
+      }))
   }
 }
