822c5c5fd9c5311eb915a336e2b3a8b50ca1efe5
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-12 22:06:53 +0530

    Adding settings module

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,3 +1,4 @@
 export {AuthModule} from './auth/auth.module'
 export {AppHomeModule} from './app-home/app-home.module'
 export {AccountModule} from './account/account.module'
+export {SettingsModule} from './settings/settings.module'
