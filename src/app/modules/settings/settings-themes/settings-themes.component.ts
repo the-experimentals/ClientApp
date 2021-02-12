9825f2dc3644156df9b2d76fda181f0d6f89a02b
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-12 22:08:47 +0530

    Settings components

---
+++ src/app/modules/settings/settings-themes/settings-themes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-themes',
+  templateUrl: './settings-themes.component.html',
+  styleUrls: ['./settings-themes.component.scss']
+})
+export class SettingsThemesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingsHomeComponent } from './settings-home/settings-home.component';
+import { SettingsThemesComponent } from './settings-themes/settings-themes.component';
+import { SettingsGeneralComponent } from './settings-general/settings-general.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SettingsHomeComponent, SettingsThemesComponent, SettingsGeneralComponent],
   imports: [
     CommonModule,
     SettingsRoutingModule
