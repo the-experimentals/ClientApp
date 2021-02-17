9dfe962d60b219ae2a79b55fcb75ce57db2b6614
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-17 16:19:00 +0530

    Account > Profile-card draft

--- src/app/modules/account/account-profile-card/account-profile-card.component.ts
+++ src/app/modules/account/account-profile-card/account-profile-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-account-profile-card',
+  selector: 'account-profile-card',
   templateUrl: './account-profile-card.component.html',
   styleUrls: ['./account-profile-card.component.scss']
 })
