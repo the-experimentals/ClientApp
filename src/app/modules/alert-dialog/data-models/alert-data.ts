2d8fadaf8fbae2938c70284cc6908dd21164874e
Bibek Saini <itsbibeksaini@gmail.com>
2021-02-26 15:41:42 +0530

    Alert dialog data

---
+++ src/app/modules/alert-dialog/data-models/alert-data.ts
@@ -0,0 +1,11 @@
+export class AlertData{
+    
+    private _MESSAGE! : string;
+    public get MESSAGE() : string {
+        return this._MESSAGE;
+    }
+    public set MESSAGE(v : string) {
+        this._MESSAGE = v;
+    }
+    
+}
