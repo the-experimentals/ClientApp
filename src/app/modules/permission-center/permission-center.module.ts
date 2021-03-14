d3075dc3942f4e1a682e20f721199a310db61b26
Bibek Saini <itsbibeksaini@gmail.com>
2021-03-14 22:12:57 +0530

    Permission center module

---
+++ src/app/modules/permission-center/permission-center.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PermissionCenterRoutingModule } from './permission-center-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PermissionCenterRoutingModule
+  ]
+})
+export class PermissionCenterModule { }
