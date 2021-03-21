4a1d176aaf7df8d052a21d0a5089f9e6e311254c
Bibek Saini <itsbibeksaini@gmail.com>
2021-03-21 22:22:33 +0530

    Adding permission centre module

---
+++ src/app/modules/permission-centre/permission-centre.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PermissionCentreRoutingModule } from './permission-centre-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PermissionCentreRoutingModule
+  ]
+})
+export class PermissionCentreModule { }
