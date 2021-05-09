0a4ce7b50f91c852f98f4dc147fbd5b1d244fcd5
Bibek Saini <itsbibeksaini@gmail.com>
2021-05-09 14:34:26 +0530

    Correcting SettingStore name

--- src/app/modules/settings/state/settings-query.ts
+++ src/app/modules/settings/state/settings-query.ts
@@ -1,8 +1,12 @@
+import { Injectable } from "@angular/core";
 import { Query } from "@datorama/akita";
 import { SettingsState } from "./settings-state";
 import { SettingsStore } from "./settings-store";
 
-export class StoreQuery extends Query<SettingsState>{
+@Injectable({
+    providedIn:'root'
+})
+export class SettingsQuery extends Query<SettingsState>{
     constructor(private settingsStore:SettingsStore){
         super(settingsStore)
     }
