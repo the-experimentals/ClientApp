384753f83c8c30d613e32812dc41297a905159b6
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-24 22:08:21 +0530

    Onboarding home and get started components

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -20,8 +20,11 @@ import {MatSelectModule} from '@angular/material/select';
 import { MatRippleModule } from '@angular/material/core';
 import { CustomSelectListModule } from '../custom-select-list/custom-select-list.module';
 import { AccountOnboardingComponent } from './account-onboarding/account-onboarding.component';
+import { AccountOnboardingHomeComponent } from './account-onboarding-home/account-onboarding-home.component';
+import { OnboardingHomeComponent } from './onboarding/onboarding-home/onboarding-home.component';
+import { OnboardingGetStartedComponent } from './onboarding/onboarding-get-started/onboarding-get-started.component';
 @NgModule({
-  declarations: [AccountChangePasswordComponent, AccountDashboardComponent, AccountNewProfileComponent, AccountProfileCardComponent, AccountProfileViewComponent, AccountOnboardingComponent],
+  declarations: [AccountChangePasswordComponent, AccountDashboardComponent, AccountNewProfileComponent, AccountProfileCardComponent, AccountProfileViewComponent, AccountOnboardingComponent, AccountOnboardingHomeComponent, OnboardingHomeComponent, OnboardingGetStartedComponent],
   imports: [
     CommonModule,
     AccountRoutingModule,
---
+++ src/app/modules/account/onboarding/onboarding-home/onboarding-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding-home',
+  templateUrl: './onboarding-home.component.html',
+  styleUrls: ['./onboarding-home.component.scss']
+})
+export class OnboardingHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
