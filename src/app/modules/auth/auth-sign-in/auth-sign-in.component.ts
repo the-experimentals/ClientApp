d02b8a52e8aaf7554f7450d419f543822d5d417d
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-04 17:11:34 +0530

    Update

--- src/app/modules/auth/auth-sign-in/auth-sign-in.component.ts
+++ src/app/modules/auth/auth-sign-in/auth-sign-in.component.ts
@@ -111,11 +111,6 @@ export class AuthSignInComponent implements OnInit {
 
   signIn(){
 
-
-    // let alertData: AlertData = new AlertData();
-    // alertData.MESSAGE = "The password for this account has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it immediately!"
-    // const dialogRef = this.alertDialogService.warningAlert(alertData)
-
     this.isSignInAttempt = true;    
     if(this.signInForm.valid){
       this.signInError = [];
@@ -124,19 +119,24 @@ export class AuthSignInComponent implements OnInit {
       this.authService.login(this.signInForm.value).subscribe(res =>{
         this.dyanamicContentLoading.hideComponent();
         if(res.IS_AUTHENTICATED){        
-          if(res.HAS_PWNED_PASSWORD){
-            let alertData: AlertData = new AlertData();
-            alertData.MESSAGE = "The password for this account has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it immediately!"
-
-            const dialogRef = this.alertDialogService.warningAlert(alertData)
-            dialogRef.afterClosed().subscribe(res =>{
-              this.router.navigate(['/home'])     
-            })
+          if(!res.IS_VERIFIED)
+          {
+            this.router.navigate(['/account/onboard'])
           }
           else{
-            this.router.navigate(['/home'])               
+            if(res.HAS_PWNED_PASSWORD){
+              let alertData: AlertData = new AlertData();
+              alertData.MESSAGE = "The password for this account has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it immediately!"
+  
+              const dialogRef = this.alertDialogService.warningAlert(alertData)
+              dialogRef.afterClosed().subscribe(res =>{
+                this.router.navigate(['/home'])     
+              })
+            }
+            else{
+              this.router.navigate(['/home'])               
+            }
           }
-            
         }          
       }, err =>{
         if(err.status == StatusCodes.UNAUTHORIZED){
