fa0a7b0eebfb2b2c446ed5b0e2dd49730d8f42a5
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-05 21:12:39 +0530

    Set profile verified after successful verification

--- src/app/modules/onboarding/onboarding-verify-account/onboarding-verify-account.component.ts
+++ src/app/modules/onboarding/onboarding-verify-account/onboarding-verify-account.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { VERIFY_PROFILE } from 'src/app/core/constants/actions/account';
 import { ACCOUNT } from 'src/app/core/constants/controllers';
 import { AlertDialogService, DyanamicContentLoadingService, HttpHelperService } from 'src/app/core/services';
+import { ProfileStore } from 'src/app/core/state/store';
+import { Profile } from 'src/app/data-models/account';
 import { VerifyProfileResponse } from 'src/app/response-models/onboarding/verify-profile-response';
 import { LoadingIndicatorComponent } from 'src/app/shared/loading-indicator/loading-indicator.component';
 import { AlertData } from '../../alert-dialog/data-models/alert-data';
@@ -20,7 +22,8 @@ export class OnboardingVerifyAccountComponent implements OnInit {
               private httpHelper:HttpHelperService, 
               private dyanamicContentLoading: DyanamicContentLoadingService,
               @Inject(ViewContainerRef) ViewContainerRef:ViewContainerRef,
-              private alertDialogService:AlertDialogService) { 
+              private alertDialogService:AlertDialogService,
+              private profileStore: ProfileStore) { 
     this.verificationCodeForm = this.createVerificationFrom();
 
     this.dyanamicContentLoading.setRootViewContainerRef(ViewContainerRef);
@@ -48,22 +51,28 @@ export class OnboardingVerifyAccountComponent implements OnInit {
 
       this.dyanamicContentLoading.showComponent(LoadingIndicatorComponent);
       this.httpHelper.post<VerifyProfileResponse>(VERIFY_PROFILE, ACCOUNT, {'OTP': enteredCode}).subscribe(res =>{
-        this.dyanamicContentLoading.hideComponent();
-        debugger
+        this.dyanamicContentLoading.hideComponent();        
         if(res.VERIFIED){
-
           let alertData: AlertData = new AlertData();
           alertData.MESSAGE = "Congrats! You are now verified."
+
+          this.profileStore.update(state => {
+            let updatedProfile:Profile = Object.assign(new Profile(), state.profile)
+            updatedProfile.IS_VERIFIED = true
+
+            localStorage.removeItem('currentUser');
+            localStorage.setItem("currentUser", JSON.stringify(updatedProfile));              
+
+            return {
+              profile: updatedProfile
+            }
+          })
 
           const dialogRef = this.alertDialogService.successAlert(alertData)
           dialogRef.afterClosed().subscribe(res =>{    
             this.changeStep.emit()     
-          })
-          
-        }
-
-
-        
+          })          
+        }        
       });
       
     }    
