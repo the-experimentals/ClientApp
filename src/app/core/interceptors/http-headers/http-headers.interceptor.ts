017fc71f56a5f15bd70988c256598f5f125bc423
Bibek Saini <itsbibeksaini@gmail.com>
2022-09-02 23:54:10 +0530

    chore: HTTP headers interceptor to add global headers

---
+++ src/app/core/interceptors/http-headers/http-headers.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    request = request.clone({
+      headers: request.headers.append("Access-Control-Allow-Origin", "*")
+    })
+
+    return next.handle(request);
+  }
+}
